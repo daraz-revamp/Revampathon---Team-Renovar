23cedaf50145d994d6679aafd452e526d2d23fb5
shresthasumesh13@gmail.com <shresthasumesh13@gmail.com>
2023-12-26 23:58:06 +0545

    Navbar and Sidebar Refinement

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function showSideBar() {
   document.getElementById("sidebar").style.display = "block";
 }
+
+function closeSideBar() {
+  document.getElementById("sidebar").style.display = "none";
+}
+
+function toggleDropdown(button) {
+  //   let btn = a.firstChild;
+  //   btn.classList.toggle("active");
+  console.log(button.firstChild);
+}
