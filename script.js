f3afb974356390750534f2a7d3f227880eab5a26
shresthasumesh13@gmail.com <shresthasumesh13@gmail.com>
2023-12-27 23:28:26 +0545

    Navbar Refinement + Add to Card implemented

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
+// function showSideBar() {
+//   document.getElementById("sidebar").style.display = "block";
+// }
+
+// function closeSideBar() {
+//   document.getElementById("sidebar").style.display = "none";
+// }
+
+let counter = document.getElementById("item-counter");
+counter.innerText = "0";
+
+function toggleDropdown(a) {
+  a.classList.toggle("active");
+  if (a.classList.contains("active")) {
+    a.innerText = "See All v";
+  } else {
+    a.innerText = "See All >";
+  }
+}
+
 function showSideBar() {
-  document.getElementById("sidebar").style.display = "block";
+  const sidebar = document.querySelector(".sidebar");
+  const currentLeft = parseInt(getComputedStyle(sidebar).left, 10);
+
+  sidebar.style.left = currentLeft === 0 ? "-350px" : "0";
 }
 
 function closeSideBar() {
-  document.getElementById("sidebar").style.display = "none";
+  document.querySelector(".sidebar").style.left = "-350px";
 }
 
-function toggleDropdown(button) {
-  //   let btn = a.firstChild;
-  //   btn.classList.toggle("active");
-  console.log(button.firstChild);
+function add_item() {
+  counter.innerText = Number(counter.innerText) + 1;
 }
