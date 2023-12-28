10352da8d2ec5b7aac8aca4dc9293a6ab84a7f7d
shresthasumesh13@gmail.com <shresthasumesh13@gmail.com>
2023-12-28 14:03:39 +0545

    flash sale added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // }
 
 let counter = document.getElementById("item-counter");
-counter.innerText = "0";
+counter.innerText = sessionStorage.getItem("item_counter");
 
 function toggleDropdown(a) {
   a.classList.toggle("active");
@@ -28,10 +28,18 @@ function showSideBar() {
 function closeSideBar() {
   document.querySelector(".sidebar").style.left = "-350px";
 }
-
-function add_item() {
-  counter.innerText = Number(counter.innerText) + 1;
-}
+let a2c_buttons = document.querySelectorAll(".add-to-cart");
+let temp = 0;
+console.log(a2c_buttons);
+a2c_buttons.forEach(function (item) {
+  item.addEventListener("click", () => {
+    console.log("kwek");
+    temp = Number(sessionStorage.getItem("item_counter"));
+    temp += 1;
+    sessionStorage.setItem("item_counter", temp);
+    counter.innerText = sessionStorage.getItem("item_counter");
+  });
+});
 
 // CAROUSEL SCRIPT
 
@@ -53,3 +61,33 @@ function prevSlide() {
   currentIndex = (currentIndex - 1 + totalItems) % totalItems;
   updateCarousel();
 }
+
+// Counter Script
+
+const targetDate = new Date();
+targetDate.setHours(targetDate.getHours() + 2); // Example: 2 hours from now
+
+function updateCounter() {
+  const currentDate = new Date();
+  const timeDifference = new Date(targetDate - currentDate);
+
+  const hours = formatTime(timeDifference.getUTCHours());
+  const minutes = formatTime(timeDifference.getUTCMinutes());
+  const seconds = formatTime(timeDifference.getUTCSeconds());
+
+  document.getElementById(
+    "counter"
+  ).textContent = `${hours}h : ${minutes}m : ${seconds}s`;
+
+  if (timeDifference <= 0) {
+    clearInterval(timer);
+    document.getElementById("counter").textContent = "Countdown Expired";
+  }
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+updateCounter(); // Initial call to avoid delay
+const timer = setInterval(updateCounter, 1000);
